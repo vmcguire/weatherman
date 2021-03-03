f12f3d4fe82f711cc7be4d1bb132aaba1f92758f
vmcguire <victor.mcguire@gmail.com>
2021-03-03 03:28:08 -0800

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//AS A traveler
+//I WANT to see the weather outlook for multiple cities
+//SO THAT I can plan a trip accordingly
+
+// GIVEN a weather dashboard with form inputs
+//1a WHEN I search for a city
+//1b THEN I am presented with current and future conditions for
+//1b that city and that city is added to the search history
+
+//2a WHEN I view current weather conditions for that city
+//2b THEN I am presented with the city name, the date, an icon
+//2b representation of weather conditions, the temperature, the humidity,
+//2b the wind speed, and the UV index
+
+//3a WHEN I view the UV index
+//3b THEN I am presented with a color that indicates whether the conditions are
+//3b favorable, moderate, or severe
+
+//4a WHEN I view future weather conditions for that city
+//4b THEN I am presented with a 5-day forecast that displays the date,
+//4b an icon representation of weather conditions, the temperature, and the humidity
+
+//5a WHEN I click on a city in the search history
+//5b THEN I am again presented with current and future
+//5b conditions for that city
